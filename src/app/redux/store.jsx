4a5d09414f8f437ff4ed482d86a853c882b03309
Mccel Villanueva MacOS <mccel.villanueva1103@gmail.com>
2025-12-01 17:17:46 +0800

    refactor: remove unused Redux providers and registration slice

--- src/app/redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import registrationReducer from "./action/registrationSlice";
-
-export const store = configureStore({
-  reducer: {
-    registration: registrationReducer
-  }
-});
