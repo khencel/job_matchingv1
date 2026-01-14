5be6b39babb88ce78b4bbaf82304d28e093a799b
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-01-14 21:22:14 +0800

    feat: enhance development script and refactor JobSearchFilter component layout

--- src/components/JobSearchFilter.tsx
+++ src/components/JobSearchFilter.tsx
@@ -1,16 +1,19 @@
-
 export default function JobSearchFiler() {
   return (
     <>
-      <div className="row m-0 job-search-filter justify-content-center p-3">
-        <div className="col-md-6 bg-white rounded search-filter-box">
+      <div className="job-search-filter d-flex justify-content-center p-3">
+        <div className="w-75 bg-white rounded search-filter-box">
           <div className="row g-2 p-1">
             <div className="col-md-5">
               <div className="input-group">
                 <span className="input-group-text">
                   <i className="bi bi-briefcase"></i>
                 </span>
-                <input type="text" className="form-control job-type" placeholder="Job Type" />
+                <input
+                  type="text"
+                  className="form-control job-type"
+                  placeholder="Job Type"
+                />
               </div>
             </div>
 
@@ -20,7 +23,9 @@ export default function JobSearchFiler() {
                   <i className="bi bi-geo-alt"></i>
                 </span>
                 <select className="form-select" defaultValue="">
-                  <option value="" disabled hidden>Select Region</option>
+                  <option value="" disabled hidden>
+                    Select Region
+                  </option>
                   <option value="test1">test1</option>
                   <option value="test2">test2</option>
                   <option value="test3">test3</option>
@@ -29,10 +34,11 @@ export default function JobSearchFiler() {
               </div>
             </div>
 
-            <div className="col-md-2 text-center">
-              <button className="btn w-100 btn-primary-custom rounded-4">Explore</button>
+            <div className="col-md-2">
+              <button className="btn btn-primary-custom w-100 rounded-4">
+                Search
+              </button>
             </div>
-
           </div>
         </div>
       </div>
