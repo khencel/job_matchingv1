1a5247eb334c1ea29af867592919e87e06b52895
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-01-16 15:28:09 +0800

    feat: implement AccountSettingsPage with edit mode for password change

--- src/app/(with-auth)/super-visory/(dashboard)/settings/page.tsx
+++ src/app/(with-auth)/super-visory/(dashboard)/settings/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import { useState } from "react";
+import { Button, CardBody, Container, Card } from "react-bootstrap";
+
 export default function AccountSettingsPage() {
-  return <div>SuperVisory Trainings Page</div>;
+  const [editMode, setEditMode] = useState(false);
+
+  return (
+    <Container fluid className="p-0">
+      <Card className="border-0">
+        <CardBody>
+          <div className="d-flex justify-content-end mb-3">
+            <Button
+              className={`${editMode ? "btn-danger" : "btn-primary"}`}
+              style={{ width: "100px" }}
+              onClick={() => setEditMode((v) => !v)}
+            >
+              {editMode ? "Cancel" : "Change Password"}
+            </Button>
+          </div>
+
+          {editMode ? <></> : <></>}
+        </CardBody>
+      </Card>
+    </Container>
+  );
 }
