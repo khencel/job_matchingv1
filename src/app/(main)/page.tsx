f05bbdcc94b59b3065c96286e894ee6cefe7f723
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-01-23 15:47:08 +0800

    feat: fetch and display job postings in JobPost component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,30 +10,15 @@ import JobSearchFiler from "@/components/JobSearchFilter";
 import Registration from "../../components/registration/Registration";
 import "animate.css";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import type { RootState } from "@/redux/store";
-import { useAppDispatch } from "@/redux/hooks";
-import { showAllJobs } from "@/redux/slices/employer/post_a_job/jobListingThunk";
-
-
 
 export default function HomePage() {
-
-  const {items} = useSelector((state:RootState) => state.jobListing)
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(showAllJobs());
-  }, []);
-
   useEffect(() => {
     (async () => {
       const { WOW } = await import("wowjs");
       new WOW({ live: false }).init();
     })();
   }, []);
-  
-  
+
   return (
     <div>
       <Navbar />
@@ -43,7 +28,7 @@ export default function HomePage() {
       <Registration />
       <ServiceContent />
       <hr className="mt-5 w-75 mx-auto" />
-      <JobPost data={items} />
+      <JobPost />
       <hr className="mt-5 w-75 mx-auto" />
       <About />
       <Footer />
--- src/components/JobPost.tsx
+++ src/components/JobPost.tsx
@@ -1,13 +1,44 @@
 import Link from "next/link";
+import JobCard from "./JobCard";
+import { useEffect, useState } from "react";
+import { getJobPostings } from "@/redux/slices/jobs/jobServices";
+import { JobPosting } from "@/types/applyJob";
+import { useRouter } from "next/navigation";
 
 export default function JobPost() {
-    // TODO: Fetch and display job postings here
+  const router = useRouter();
+  const [jobPostings, setJobPostings] = useState<JobPosting[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { WOW } = await import("wowjs");
+      new WOW({ live: false }).init();
+    })();
+  }, []);
+
+  useEffect(() => {
+    async function fetchJobDetails() {
+      const res = await getJobPostings();
+      setJobPostings(res.data);
+    }
+    fetchJobDetails();
+  }, []);
+
   return (
     <div className="container py-4">
       <div className="row justify-content-center wow animate__animated animate__fadeInUp">
         <div className="col-md-10">
           <div className="row justify-content-center">
-            <div className="row "></div>
+            <div className="row ">
+              {jobPostings.slice(0, 3).map((job) => (
+                <div className="col-md-4 mb-4 d-flex" key={job.id}>
+                  <JobCard
+                    job={job}
+                    onClick={() => router.push(`job-description/${job.id}`)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
           <div className="row mt-4">
             <div className="col">
