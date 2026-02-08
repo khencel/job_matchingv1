da30ffaf578f1ab1e1b3236831afef3c1a3ce73e
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-02-08 19:00:08 +0800

    components folder and employer pages translations

--- src/messages/en/index.ts
+++ src/messages/en/index.ts
@@ -2,7 +2,70 @@ import common from "./common.json";
 import navbar from "./navbar.json";
 import auth from "./auth.json";
 import banner from "./banner.json";
+import about from "./About.json";
+import serviceContent from "./ServiceContent.json";
+import footer from "./Footer.json";
 import jobSearchFilter from "./jobSearchFilter.json";
+import filterJobs from "./FilterJobs.json";
+import jobCard from "./JobCard.json";
+import jobPost from "./JobPost.json";
+import publicProfileJobSeeker from "./PublicProfileJobSeeker.json";
+import displayResume from "./DisplayResume.json";
+import displayDocuments from "./DisplayDocuments.json";
+import previewFile from "./PreviewFile.json";
+import spinner from "./Spinner.json";
+import multipleSelectStandard from "./MultipleSelectStandard.json";
+import registration from "./Registration.json";
+import listGroupData from "./listGroupData.json";
+import dateFormat from "./date_format.json";
+import jobSupportFeatures from "./jobSupportFeatures.json";
+import contactUs from "./contactUs.json";
+import qAndA from "./qAndA.json";
+import login from "./login.json";
+import register from "./register.json";
+import findJobs from "./find-jobs.json";
+import adminSidebar from "./adminSidebar.json";
+import adminOverview from "./adminOverview.json";
+import adminUsers from "./adminUsers.json";
+import adminApplicants from "./adminApplicants.json";
+import adminApplicantsFilterModal from "./adminApplicantsFilterModal.json";
+import adminApplicantsViewEmployer from "./adminApplicantsViewEmployer.json";
+import adminApplicantsViewApplicant from "./adminApplicantsViewApplicant.json";
+import adminSettings from "./adminSettings.json";
+import superVisoryDashboardLayout from "./superVisoryDashboardLayout.json";
+import superVisoryOverview from "./superVisoryOverview.json";
+import superVisoryApplicants from "./superVisoryApplicants.json";
+import jobSeekerDashboardLayout from "./jobSeekerDashboardLayout.json";
+import jobSeekerProfileDashboard from "./jobSeekerProfileDashboard.json";
+import jobSeekerAppliedJobs from "./jobSeekerAppliedJobs.json";
+import jobSeekerDocuments from "./jobSeekerDocuments.json";
+import resumeBuilder from "./resumeBuilder.json";
+import resumeForm from "./resumeForm.json";
+import resumeTemplate from "./resumeTemplate.json";
+import jobListFilter from "./jobListFilter.json";
+import jobListCard from "./jobListCard.json";
+import employerOverview from "./employerOverview.json";
+import employerOverviewChart from "./employerOverviewChart.json";
+import employerOverviewRecentPostJob from "./employerOverviewRecentPostJob.json";
+import employerOverviewRecentActivity from "./employerOverviewRecentActivity.json";
+import employerOverviewStatusActivity from "./employerOverviewStatusActivity.json";
+import employerProfileHeader from "./employerProfileHeader.json";
+import employerProfileBody from "./employerProfileBody.json";
+import employerProfileTeam from "./employerProfileTeam.json";
+import employerProfileEditModal from "./employerProfileEditModal.json";
+import employerPerksBenefits from "./employerPerksBenefits.json";
+import employerPerksBenefitsPage from "./employerPerksBenefitsPage.json";
+import employerPerksAddModal from "./employerPerksAddModal.json";
+import employerPerksEditModal from "./employerPerksEditModal.json";
+import employerJobListing from "./employerJobListing.json";
+import employerJobListingEditModal from "./employerJobListingEditModal.json";
+import employerPostJobInformation from "./employerPostJobInformation.json";
+import employerPostJobDescription from "./employerPostJobDescription.json";
+import employerPostJobPerksBenefit from "./employerPostJobPerksBenefit.json";
+import employerPostJobAddBenefitModal from "./employerPostJobAddBenefitModal.json";
+import employerPostJobHeader from "./employerPostJobHeader.json";
+import employerApplicants from "./employerApplicants.json";
+import employerAddSkill from "./employerAddSkill.json";
 import registerButton from "./registerButton.json";
 import registrationModal from "./registrationModal.json";
 import forgotPasswordPage from "./forgotPasswordPage.json";
@@ -26,7 +89,70 @@ const messages = {
   navbar,
   auth,
   banner,
+  about,
+  serviceContent,
+  footer,
   jobSearchFilter,
+  filterJobs,
+  jobCard,
+  jobPost,
+  publicProfileJobSeeker,
+  displayResume,
+  displayDocuments,
+  previewFile,
+  spinner,
+  multipleSelectStandard,
+  registration,
+  listGroupData,
+  dateFormat,
+  jobSupportFeatures,
+  contactUs,
+  qAndA,
+  login,
+  register,
+  findJobs,
+  adminSidebar,
+  adminOverview,
+  adminUsers,
+  adminApplicants,
+  adminApplicantsFilterModal,
+  adminApplicantsViewEmployer,
+  adminApplicantsViewApplicant,
+  adminSettings,
+  superVisoryDashboardLayout,
+  superVisoryOverview,
+  superVisoryApplicants,
+  jobSeekerDashboardLayout,
+  jobSeekerProfileDashboard,
+  jobSeekerAppliedJobs,
+  jobSeekerDocuments,
+  resumeBuilder,
+  resumeForm,
+  resumeTemplate,
+  jobListFilter,
+  jobListCard,
+  employerOverview,
+  employerOverviewChart,
+  employerOverviewRecentPostJob,
+  employerOverviewRecentActivity,
+  employerOverviewStatusActivity,
+  employerProfileHeader,
+  employerProfileBody,
+  employerProfileTeam,
+  employerProfileEditModal,
+  employerPerksBenefits,
+  employerPerksBenefitsPage,
+  employerPerksAddModal,
+  employerPerksEditModal,
+  employerJobListing,
+  employerJobListingEditModal,
+  employerPostJobInformation,
+  employerPostJobDescription,
+  employerPostJobPerksBenefit,
+  employerPostJobAddBenefitModal,
+  employerPostJobHeader,
+  employerApplicants,
+  employerAddSkill,
   registerButton,
   registrationModal,
   forgotPasswordPage,
