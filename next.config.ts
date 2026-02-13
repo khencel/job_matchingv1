e8bd0b70f4443e998cdb0864793a002e63160983
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-02-13 14:03:27 +0800

    fix config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         // Proxy API requests to the backend server
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`, // HTTP is okay here
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}:path*`, // HTTP is okay here
       },
       {
         // Proxy media requests to the backend server
