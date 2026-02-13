42543a1fdf232ddd726c43c599cfa3b72da4cea2
Mccel Villanueva <mccel.villanueva1103@gmail.com>
2026-02-13 12:36:35 +0800

    update config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_CONTENT_URL}/:path*`, // HTTP is okay here
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`, // HTTP is okay here
       },
     ];
   },
